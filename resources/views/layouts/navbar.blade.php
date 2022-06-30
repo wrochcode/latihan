9d1faf9471348eb4e0b50abb370c9b181cdbe8df
Wahyu Rochman <wrochcode@gmail.com>
2022-06-30 14:18:38 +0700

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,6 +18,16 @@
                 <a class="nav-link" href="{{ $url }}">{{ $item }}</a>
               </li>
             @endforeach
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
         </ul>
       </div>
     </div>
